c46e75d5c9b13088bc5512508c0663211fe9a635
Harsh Soni <sonie.harsh29@gmail.com>
2023-12-24 16:06:21 +0530

    NewProject Added Password Generator Completed

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -1,12 +1,14 @@
-import { useState ,useCallback} from 'react'
+import { useState ,useCallback,useEffect,useRef} from 'react'
 
-import './App.css'
+// import './App.css'
 
 function App() {
   const [length, setLength] = useState(8);
   const [numberAllowed,setNumberAllowed]=useState(false);
   const[charAllowed,setCharAllowed]=useState(false);
   const[password,setPassword]=useState("");
+  //useRef hook
+  const passwordRef=useRef(null);
 
   const passwordGenerator=useCallback(()=>{
     let pass="";
@@ -14,19 +16,77 @@ function App() {
     if(numberAllowed) str+="0123456789"
     if(charAllowed) str+="!@##$%^&*(){}[]:;"
 
-    for(let i=0;i<=array.length;i++){
-      let char=Math.floor(Math.random*str.length+1);
-      pass=str.charAt(char);
+    for(let i=1;i<=length;i++){
+      let char=Math.floor(Math.random() * str.length+1);
+      pass+=str.charAt(char);
 
     }
     setPassword(pass);
   },[length,numberAllowed,charAllowed,setPassword])
-
+  const copyPasswordToClipboard=useCallback(() =>{
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    window.navigator.clipboard.writeText(password);
+  },[password]);
+  useEffect(()=>{
+    passwordGenerator();
+  },[length,numberAllowed,charAllowed,passwordGenerator])
+//useCallBack is for function optimization and memomization
+//useEffect  like inme se koi bhi chedchad ho toh dubara se run krdo
   return (
-    <>
-      <h1 className='text-4xl text-center text text-white' >Password Generator</h1>
-      <div className='w-full max-w-md '></div>
-      </>
+  
+      
+      <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-700'><h1 className=' text-center text text-white my-4' >Password Generator</h1>
+      <div className="flex shadow rounded-lg overflow-hidden mb-4 ">
+        <input
+        type="text"
+        value={password}
+        className='outline-none w-full py-1 px-3'
+        placeholder='password'
+        readOnly
+        ref={passwordRef}
+  />
+  <button 
+  onClick={copyPasswordToClipboard}
+  className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'>copy</button>
+      </div>
+      <div className='flex text-sm gap-x-2'
+      >
+        <div className='flex items-center gap-x-1'>
+          <input
+          type="range"
+          min={6}
+          max={100}
+          value={length}
+          className='cursor-pointer'
+          onChange={(e)=>{setLength(e.target.value)}}
+          />
+          <label>Length:{length}</label>
+        </div>
+        <div className='flex items-center gap-x-1'>
+          <input
+          type="checkbox"
+          defaultChecked={numberAllowed}
+          id="numberInput"
+          onChange={()=>{setNumberAllowed((prev)=>!prev)}}//prev value ko reversh kr diya means true and false hota rhega!!
+          />
+          <label htmlFor="numberInput">Numbers</label>
+        </div>
+        <div className='flex items-center gap-x-1'>
+          <input
+          type="checkbox"
+          defaultChecked={charAllowed}
+          id="characterInput"
+          onChange={()=>{setCharAllowed((prev)=>!prev)}}//prev value ko reversh kr diya means true and false hota rhega!!
+          //if  we do this liek setCharAllowed(true ) then when onchange called value set as true but that is not we want
+          //so to propogate events we can fir callbacks  iss call back me hme previous value the access milta hh jp prev mila usko 
+          //new value dene ke liye unlta kr diya 
+          />
+          <label htmlFor="characterInput">Characters</label>
+        </div>
+      </div>
+</div>
+      
   )
 }
 
